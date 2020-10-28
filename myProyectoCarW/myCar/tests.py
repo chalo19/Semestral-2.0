7de9713d62d8251672cd686b604966413a1f5b17
Chalo19 <gonzalobravotobar@gmail.com>
2020-10-28 14:07:53 -0300

    proyecto sin test

--- myProyectoCarW/myCar/tests.py
+++ myProyectoCarW/myCar/tests.py
@@ -1,25 +1,3 @@
 from django.test import TestCase
-import unittest
-from .models import MisionyVision
 
 # Create your tests here.
-
-def grabar_mision_y_vision(self):
-        m = MisionyVision(
-            mision="Garantizar la satisfacción y servicio de excelencia, ofreciendo una alternativa para maximizar el tiempo del cliente, creando la combinación perfecta entre servicio y calidad que provoque la lealtad de nuestros clientes."
-            ,vision="Ser una de las empresas más eficientes para el lavado de automóviles en Chile, ofreciendo a cada uno de nuestros clientes soluciones prácticas para el aseo de sus vehículos con la calidad, servicio y compromiso que nos caracteriza, y así poder garantizar la lealtad del consumidor."
-        )
-        valor=0
-        try:
-            m.save()
-            valor = 1
-        except:
-            valor = 0
-        self.assertEqual(valor,1)
-
-def listar_mision(self):
-    lm= MisionyVision.objects.all()
-    self.assertIsInstance(lm,MisionyVision)
-        
-if __name__ == "__main__":
-    unittest.main()
